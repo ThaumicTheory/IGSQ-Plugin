21da7b89b88616033913e68fb66298a88d01b23f
arynxd <64087640+arynxd@users.noreply.github.com>
2020-07-18 17:25:02 +0100

    added more to lightning
    improved lightning

--- src/me/murrobby/igsq/spigot/expert/ThunderChangeEvent_Expert.java
+++ src/me/murrobby/igsq/spigot/expert/ThunderChangeEvent_Expert.java
@@ -2,10 +2,14 @@ package me.murrobby.igsq.spigot.expert;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.io.IOException;
 import java.util.Random;
 
 import me.murrobby.igsq.spigot.Common_Spigot;
@@ -18,6 +22,10 @@ public class ThunderChangeEvent_Expert implements Listener
 	Random random = new Random();
 	private int randomX;
 	private int randomZ;
+	private EntityType entitytype;
+	private Player selectedPlayer;
+	private World world;
+	
 	
 	
 	public ThunderChangeEvent_Expert(Main_Spigot plugin)
@@ -35,46 +43,85 @@ public class ThunderChangeEvent_Expert implements Listener
 		{
 			if(event.toThunderState())
 			{
-				thunderTask = plugin.scheduler.scheduleSyncRepeatingTask(plugin, new Runnable()
-		    	{
-
-					@Override
-					public void run() 
+				try {
+					Common_Spigot.internalData.set(".event.lightning",true);
+					Common_Spigot.internalData.save(Common_Spigot.internalDataFile);
+					ExtraLightning(world, event);
+					Creepers(selectedPlayer);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else 
+			{
+				try {
+					Common_Spigot.internalData.set(".event.lightning",false);
+					Common_Spigot.internalData.save(Common_Spigot.internalDataFile);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			}
+		}
+	
+	public void ExtraLightning(World world, org.bukkit.event.weather.ThunderChangeEvent event) {
+		if(Common_Spigot.getFieldBool(".event.lightning", "internal")){
+			thunderTask = plugin.scheduler.scheduleSyncRepeatingTask(plugin, new Runnable()
+			{
+		
+				@Override
+				public void run() 
+				{
+					for(Player selectedPlayer : plugin.getServer().getOnlinePlayers()) 
 					{
-						for(Player selectedPlayer : plugin.getServer().getOnlinePlayers()) 
+						if(selectedPlayer.getWorld() == event.getWorld())
 						{
-							if(selectedPlayer.getWorld() == event.getWorld())
+							if(random.nextInt(3) == 1)
 							{
-								if(random.nextInt(3) == 1)
+								randomX = random.nextInt(16);
+								randomZ = random.nextInt(16);
+								Location location = selectedPlayer.getLocation();
+								location.add(randomX, 0 , randomZ);
+								try 
 								{
-									randomX = random.nextInt(16);
-									randomZ = random.nextInt(16);
-									Location location = selectedPlayer.getLocation();
-									location.add(randomX, 0 , randomZ);
-									try 
-									{
-										Block target = Common_Spigot.GetHighestBlock(location);
-										location = target.getLocation().add(0,1,0);
-										event.getWorld().strikeLightning(location);
-									}
-									catch(NullPointerException e) 
-									{
-										//Failed To Find Spot For Lightning
-									}
+									Block target = Common_Spigot.GetHighestBlock(location);
+									location = target.getLocation().add(0,1,0);
+									event.getWorld().strikeLightning(location);
+								}
+								catch(NullPointerException e) 
+								{
+									//Failed To Find Spot For Lightning
 								}
 							}
 						}
-					} 		
-		    	}, 0, 20);
-			}
-			else if(thunderTask != -1){
-				plugin.scheduler.cancelTask(thunderTask);
-			}
+					}
+				} 		
+			}, 0, 20);
 		}
-		else if(thunderTask != -1) {
-			plugin.scheduler.cancelTask(thunderTask);
+		else {
+			Common_Spigot.internalData.set(world.getUID() + ".event.lightning",false);
 		}
 		
 	}
 	
+	private void Creepers(Player selectedPlayer) {
+		if(Common_Spigot.getFieldBool(".event.lightning", "internal"))
+		{
+			entitytype = EntityType.CREEPER;
+			randomX = random.nextInt(16);
+			randomZ = random.nextInt(16);
+			Location location = selectedPlayer.getLocation();
+			location.add(randomX, 100 , randomZ);
+			plugin.getServer().getPlayer(selectedPlayer.getUniqueId()).getWorld().spawnEntity(location, entitytype);
+		}
+		else {
+			Common_Spigot.internalData.set(".event.lightning",false);
+		}
+	}
+	
 }
+
+	
+
+
